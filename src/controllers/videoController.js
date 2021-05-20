032990291f0e608a94258e8692db5a5c12790bb3
junsooyeo <59544981+junsooyeo@users.noreply.github.com>
2021-05-19 21:10:13 -0400

    third commitment
    build some htmls by pug (base, edit, home, video, footer, upload, watch) and playing with the fake database, I understand how get and post works.
    
    Future work will be : making a real database using mongoDB and mongoose

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,70 @@
-export const watch = (req, res) => {
-	console.log(`watching #${req.params.id}`);
-	return res.send("VIDEO WATCH");
+let videos = [
+	{
+		title: "First",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 mins ago",
+		views: 1,
+		id: 1,
+	},
+	{
+		title: "Second",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 mins ago",
+		views: 59,
+		id: 2,
+	},
+	{
+		title: "Third",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 mins ago",
+		views: 59,
+		id: 3,
+	},
+];
+const fakeUser = {
+	userName: "Junsoo",
+	loggedIn: true,
 };
-export const home = (req, res) => res.send("HOME");
-export const search = (req, res) => res.send("SEARCH VIDEOS");
 
-export const edit = (req, res) => res.send("VIDEO EDIT");
-export const remove = (req, res) => res.send("VIDEO REMOVE");
-export const upload = (req, res) => res.send("UPLOAD");
+export const home = (req, res) => {
+	return res.render("home", { pageTitle: "HOME", fakeUser, videos });
+};
+export const watch = (req, res) => {
+	const { id } = req.params;
+	const video = videos[id - 1];
+	return res.render("watch", { pageTitle: video.title, video });
+};
+
+export const getEdit = (req, res) => {
+	const { id } = req.params;
+	const video = videos[id - 1];
+	return res.render("edit", { pageTitle: `edit ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+	const { id } = req.params;
+	const { title } = req.body;
+	videos[id - 1].title = title;
+	return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+	return res.render("upload", { pageTitle: `Upload video` });
+};
+
+export const postUpload = (req, res) => {
+	const { title } = req.body;
+	const newVideo = {
+		title,
+		rating: 0,
+		comments: 0,
+		createdAt: "just now",
+		views: 0,
+		id: videos.length + 1,
+	};
+	videos.push(newVideo);
+	return res.redirect("/");
+};
--- src/server.js
+++ src/server.js
@@ -7,7 +7,10 @@ import videoRouter from "./routers/videoRouter";
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
